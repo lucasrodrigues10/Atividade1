9d38ab91b4342f6065d8e036f495d8c15839fb5b
lucas <lucasrodrigues10@outlook.com>
2019-04-07 14:47:49 -0400

    adiciona botas de compartilhar

--- Atividade1/ViewController.swift
+++ Atividade1/ViewController.swift
@@ -16,13 +16,16 @@ class ViewController: UIViewController {
     }
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if (motion == .motionShake){
-            lblCompartilharFrase.text = "# clique em mim para compartilhar"
+            lblCompartilharFrase.text = "# clique em compartilhar"
+            btnCompartilhar.isHidden = false
         }
     }
 
 
+    
+    @IBOutlet weak var btnCompartilhar: UIButton!
     @IBOutlet weak var lblCompartilharFrase: UILabel!
-    @IBOutlet weak var lblFuture: UILabel!
+    @IBOutlet weak var lblFuturo: UILabel!
     @IBAction func lerFuturo(_ sender: UIButton) {
         // numeros aleatorios
         let numAleSujeito = Int.random(in: 0 ... 2)
@@ -38,8 +41,19 @@ class ViewController: UIViewController {
         let fraseFinal = frasesSujeito[numAleSujeito]+frasesVerbo[numAleVerbo]+frasesSubstantivo[numAleSubstantivo]
         
         // label com a frase final
-        lblFuture.numberOfLines = 0
-        lblFuture.text = fraseFinal
+        lblFuturo.numberOfLines = 0
+        lblFuturo.text = fraseFinal
+    }
+    @IBAction func compartilharFraseTwitter(_ sender: Any) {
+        // pega o texto
+        let textoTweet = lblFuturo.text!
+        let fraseCompartilhar = "https://twitter.com/intent/tweet?text=\(textoTweet)"
+        
+        // cria url
+        let url = URL(string: fraseCompartilhar.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)
+        
+        // abre o navegador
+        UIApplication.shared.openURL(url!)
     }
     
 }
